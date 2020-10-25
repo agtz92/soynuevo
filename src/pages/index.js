54c82ac2052162840b5e741ec20cdb9c2e194de6
agtz92 <alfredo.gtz92@gmail.com>
2020-10-25 12:46:51 -0600

    changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,7 @@ export default function Home({data}) {
       <h2 className="heading-categoria" >Jardinería </h2>
       <Blockcontainer>
         {data.allMarkdownRemark.edges.map(({ node }) => {
-          if (node.frontmatter.categoria.includes('Jardineria')) {
+          if (node.frontmatter.categoria.includes('Jardinería')) {
             return (
               <Link key={node.id} to={node.frontmatter.slug}>
                 <PostBlock name={node.frontmatter.title} text={node.frontmatter.short_description} nivel={node.frontmatter.dificultad} background={node.frontmatter.featuredimage} />
